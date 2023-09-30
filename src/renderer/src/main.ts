8a8d110669e303f29293b8bd08b7e6149d5cb1d2
charliefei <2062753446@qq.com>
2023-09-30 15:41:57 +0800

    ⭐feat: 打包上传完成

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import ElementPlus from 'element-plus'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import './assets/css/main.scss'
 import App from './App.vue'
@@ -10,9 +9,6 @@ import App from './App.vue'
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 const app = createApp(App)
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
 app.use(ElementPlus)
 app.use(pinia)
 app.mount('#app')
